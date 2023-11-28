fc962fe14ad69b7fd7918bc07d02f8ad74772eeb
CyrilSbrodov <sbrandy@bk.ru>
2023-11-28 11:15:39 +0300

    add storage intarface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,8 @@ package storage
 import "github.com/CyrilSbrodov/ToDoList/internal/storage/models"
 
 type Storage interface {
-	GetAll() models.User
-	NewList(user models.User) error
+	NewUser(user *models.User) error
+	Auth(user *models.User) error
+	NewList(user *models.User) error
+	GetAll(user *models.User) (models.User, error)
 }
